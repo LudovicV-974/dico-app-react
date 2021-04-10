168602e075479f3a28953539437a1abf09d89dbc
LudovicV-974 <76167236+LudovicV-974@users.noreply.github.com>
2021-04-10 16:50:45 +0200

    Added Photos

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Dictionary from "./Dictionary";
 function App() {
   return (
     <div className="App">
-      <header className="app-header">Dictionnaire</header>
-     <Dictionary defaultKeyword = "run"/> 
+      <header className="app-header">Word Findr</header>
+     <Dictionary defaultKeyword = "sunset"/> 
       <footer className="text-center">Coded by Ludovic</footer>
     </div>
   );
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,23 +1,36 @@
 import React, {useState} from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props){
     let [keyword, setKeyword] = useState(props.defaultKeyword);
     let [results, setResults] = useState(null);
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState (null);
 
     function handleResponse(response){
         console.log(response.data[0]);
         setResults(response.data[0]);       
     };
 
+    function handlePexelsResponse(response){
+        console.log(response);
+        setPhotos(response.data.photos);
+    }
+
     function search(){
         //documentation: https://dictionaryapi.dev
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-        axios.get(apiUrl).then(handleResponse)
+        axios.get(apiUrl).then(handleResponse);
+
+        let pexelsApiKey = "563492ad6f91700001000001cb7516af9a0f474883d3f83d1431eb4d";
+        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=8`;
+        let headers = {Authorization:`Bearer ${pexelsApiKey}`};
+        axios.get(pexelsApiUrl, {headers: headers}).then(handlePexelsResponse);
     }
+
 
     function handleSubmit (event){
         event.preventDefault();
@@ -43,6 +56,7 @@ export default function Dictionary(props){
             </div>
             </section>
             <Results results= {results}/>
+            <Photos photos={photos}/>
         </div>
     );
   } else {
