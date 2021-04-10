d83265f6ade0cf89623ec716dd88ec68ede5204e
LudovicV-974 <76167236+LudovicV-974@users.noreply.github.com>
2021-04-10 12:16:40 +0200

    Added Phonetics, synonyms, styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -27,9 +27,14 @@ export default function Dictionary(){
 
     return (
         <div className= "Dictionary">
+            <section>
             <form onSubmit={search}> 
                 <input type="search" onChange={handleKeywordChange}/>
             </form>
+            <div className="hint">
+                suggested words: sunset, egg, plant, dog...
+            </div>
+            </section>
             <Results results= {results}/>
         </div>
     );
